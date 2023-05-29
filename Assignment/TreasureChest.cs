bb975362b39a0b8234b00f16f183e2d1281470f6
devangsuiii <130946505+devangsuiii@users.noreply.github.com>
2023-05-29 05:08:45 +0000

    c2

--- Assignment/TreasureChest.cs
+++ Assignment/TreasureChest.cs
@@ -1,5 +1,10 @@
-﻿namespace Assignment
+﻿using System;
+
+namespace Assignment
 {
+    /// <summary>
+    /// Represents a treasure chest.
+    /// </summary>
     public class TreasureChest
     {
         private State _state = State.Locked;
@@ -16,11 +21,22 @@
         }
 
         // Document these methods with XML documentation
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreasureChest"/> class with the specified state.
+        /// </summary>
+        /// <param name="state">The initial state of the treasure chest.</param>
         public TreasureChest(State state) : this()
         {
             _state = state;
         }
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="TreasureChest"/> class with the specified properties.
+        /// </summary>
+        /// <param name="material">The material of the treasure chest.</param>
+        /// <param name="lockType">The lock type of the treasure chest.</param>
+        /// <param name="lootQuality">The loot quality of the treasure chest.</param>
         public TreasureChest(Material material, LockType lockType, LootQuality lootQuality)
         {
             _material = material;
@@ -29,12 +45,21 @@
         }
 
         // This is called a getter
+        /// <summary>
+        /// Gets the current state of the treasure chest.
+        /// </summary>
+        /// <returns>The state of the treasure chest.</returns>
         public State GetState()
         {
             return _state;
         }
 
-       public State Manipulate(Action action)
+        /// <summary>
+        /// Manipulates the treasure chest based on the specified action.
+        /// </summary>
+        /// <param name="action">The action to perform on the treasure chest.</param>
+        /// <returns>The updated state of the treasure chest.</returns>
+        public State Manipulate(Action action)
         {
             switch (action)
             {
@@ -58,7 +83,9 @@
             return _state; // Return the updated state of the chest
         }
 
-
+        /// <summary>
+        /// Unlocks the treasure chest.
+        /// </summary>
         public void Unlock()
         {
             if (_state == State.Locked)
@@ -72,6 +99,9 @@
             }
         }
 
+        /// <summary>
+        /// Locks the treasure chest.
+        /// </summary>
         public void Lock()
         {
             if (_state == State.Closed)
@@ -84,9 +114,12 @@
                 Console.WriteLine("The chest must be closed before it can be locked.");
             }
         }
+
+        /// <summary>
+        /// Opens the treasure chest.
+        /// </summary>
         public void Open()
         {
-            // We should check if the chest is closed
             if (_state == State.Closed)
             {
                 _state = State.Open;
@@ -101,6 +134,9 @@
             }
         }
 
+        /// <summary>
+        /// Closes the treasure chest.
+        /// </summary>
         public void Close()
         {
             if (_state == State.Open)
@@ -114,6 +150,10 @@
             }
         }
 
+        /// <summary>
+        /// Returns a string representation of the treasure chest.
+        /// </summary>
+        /// <returns>A string representation of the treasure chest.</returns>
         public override string ToString()
         {
             return $"A {_state} chest with the following properties:\nMaterial: {_material}\nLock Type: {_lockType}\nLoot Quality: {_lootQuality}";
